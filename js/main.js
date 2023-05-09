37b1acd938d3c44fece4163d0fe282878312f64e
Oleksandr Semeniuk <ol.semeniuk.mail@gmail.com>
2023-05-09 22:50:24 +0300

    basic footer styles

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
+
 // just for test
+
 const loginButton = document.querySelector('.user__login-button');
 const logoutButton = document.querySelector('.user__logout-button');
 const header = document.querySelector('.header');
 
 loginButton.onclick = () => header.classList.add('authorized');
 logoutButton.onclick = () => header.classList.remove('authorized');
+
+// ===============================
