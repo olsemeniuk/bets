4dcb82b098e5e4f0da8bb8d541b23c7b217ba745
Oleksandr Semeniuk <ol.semeniuk.mail@gmail.com>
2023-05-09 21:42:39 +0300

    basic header styles

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// just for test
+const loginButton = document.querySelector('.user__login-button');
+const logoutButton = document.querySelector('.user__logout-button');
+const header = document.querySelector('.header');
+
+loginButton.onclick = () => header.classList.add('authorized');
+logoutButton.onclick = () => header.classList.remove('authorized');
