1b019f1352fb5a2310a98209ba01aca2b0155d05
Oleksandr Semeniuk <ol.semeniuk.mail@gmail.com>
2023-05-11 20:54:50 +0300

    bets-list__item styles

---
+++ js/bet.js
@@ -0,0 +1,25 @@
+const lastBetsList = document.querySelector('.bets-list');
+
+lastBetsList.onclick = toggleBetItems;
+
+function toggleBetItems(event) {
+  const { target } = event;
+  const toggleButton = target.closest('.bets-item__show-content');
+
+  if (!toggleButton) return;
+
+  const parentItem = toggleButton.closest('.bets-item');
+  const listWrapperToToggle = parentItem.querySelector('.bets-item__content');
+  const listToToggle = parentItem.querySelector('.bets-item__items-list');
+
+  const listHeight = listToToggle.getBoundingClientRect().height;
+  const listWrapperHeight = listWrapperToToggle.getBoundingClientRect().height;
+
+  if (listWrapperHeight === 0) {
+    listWrapperToToggle.style.height = `${listHeight}px`;
+    parentItem.classList.add('bets-item--opened');
+  } else {
+    listWrapperToToggle.style.height = 0;
+    parentItem.classList.remove('bets-item--opened');
+  }
+}
