1065fe55ffb30959991e20b7362644be1846beea
Oleksandr Semeniuk <ol.semeniuk.mail@gmail.com>
2023-05-11 18:04:26 +0300

    language select in footer

--- js/main.js
+++ js/main.js
@@ -1,11 +1,16 @@
+// imports
+import { Select } from "./Select.js";
+
+// footer languages toggle
+new Select('#footer_languages_toggle').startEvents();
+
+
 
 // just for test
-
 const loginButton = document.querySelector('.user__login-button');
 const logoutButton = document.querySelector('.user__logout-button');
 const header = document.querySelector('.header');
 
 loginButton.onclick = () => header.classList.add('authorized');
 logoutButton.onclick = () => header.classList.remove('authorized');
-
 // ===============================
